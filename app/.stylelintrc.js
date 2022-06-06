c3bd302e48b69a684323db693543a948756e56a5
sae-github <miyasae1126@gmail.com>
2022-06-06 12:56:09 +0900

    refactor: execute formatter

--- app/.stylelintrc.js
+++ app/.stylelintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   ignoreFiles: ['**/node_modules/**'],
   rules: {
-    'string-quotes': 'single',
     'selector-class-pattern': null,
     'value-no-vendor-prefix': null,
   },
