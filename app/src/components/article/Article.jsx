54c87d30dc3338d903399cde1f574f73e6a1ad69
sae-github <miyasae1126@gmail.com>
2022-06-06 15:43:08 +0900

    feat: issue9

--- app/src/components/article/Article.jsx
+++ app/src/components/article/Article.jsx
@@ -1,31 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Comment } from '../comment/Comment';
 import './Article.css';
 
 export const Article = () => {
+  const [article, setArticle] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getArticleData = async () => {
+      try {
+        const res = await fetch(`http://localhost:3001/api/posts/${id}`);
+        const json = await res.json();
+        setArticle(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getArticleData();
+  }, [id]);
+
   return (
     <article className="article">
       <div className="article__head">
-        <h1 className="article__title">タイトル</h1>
+        <h1 className="article__title">{article.title}</h1>
         <div className="article__favorite">
           <button className="article__favorite-icon"></button>
           <span className="article__favorite-text"></span>
         </div>
       </div>
-      <figure className="article__img-wrapper">
-        <img
-          src="https://i.postimg.cc/YSrXJmJ6/tech-daily-Hpek-D5ujnb-Q-unsplash.jpg"
-          alt=""
-        />
-      </figure>
-      <div className="article__text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </div>
+      {article.thumbnail && (
+        <figure className="article__img-wrapper">
+          <img src={article.thumbnail} alt="" />
+        </figure>
+      )}
+      <div className="article__text">{article.content}</div>
       <Comment />
     </article>
   );
