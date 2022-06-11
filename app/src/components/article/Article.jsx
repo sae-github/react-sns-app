3a8814e82d3be44d3adc692164dd4f26706997d3
sae-github <miyasae1126@gmail.com>
2022-06-11 22:14:09 +0900

    feat: issue21

--- app/src/components/article/Article.jsx
+++ app/src/components/article/Article.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Comment } from '../comment/Comment';
+
+import { CommentInputField } from '../commentInputField/CommentInputField';
+import { CommentList } from '../commentList/CommentList';
 import './Article.css';
 
 export const Article = () => {
@@ -35,41 +37,10 @@ export const Article = () => {
         </figure>
       )}
       <div className="article__text">{article.content}</div>
-
       <div className="article__comment">
         <p className="article__comment-head">コメント</p>
-        <div className="article__comment-field">
-          <input
-            type="text"
-            name=""
-            id=""
-            className="article__comment-input"
-            placeholder="コメントを入力する"
-          />
-          <div className="article__comment-button-wrapper">
-            <button type="submit" className="article__comment-submit" disabled>
-              投稿する
-            </button>
-            <button
-              type="button"
-              className="article__comment-reset-button"
-              disabled
-            >
-              リセット
-            </button>
-          </div>
-        </div>
-        {article.comments && article.comments.length > 0 ? (
-          <ul className="article__comment-list">
-            {article.comments.map((data) => (
-              <Comment key={data.userId} commentData={data} />
-            ))}
-          </ul>
-        ) : (
-          <p className="article__not-comment-message">
-            現在コメントはありません
-          </p>
-        )}
+        <CommentInputField id={id} />
+        <CommentList comments={article.comments} />
       </div>
     </article>
   );
